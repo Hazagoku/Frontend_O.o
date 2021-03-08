7bf659cb2aeb640145ed9c1171df4e921591ba35
Alondra De la Rosa <180300367@ucaribe.edu.mx>
2021-03-07 19:16:23 -0500

    Formulario
    creo que asi esta, es el formulario para crear fichas de autos. Verion 1

---
+++ FORMULARIO/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Formulario from './Componente/Formulario'
+
+
+
+function App() {
+  return (
+    <div className ="container mt-5">
+      <Formulario />
+    </div>
+  )
+}
+
+export default App;
