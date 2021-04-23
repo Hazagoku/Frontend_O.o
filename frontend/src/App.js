c207c6ce7d59e741b43e16e5118d7edb53cfab74
ulises-lopez-diaz <69325727+ulises-lopez-diaz@users.noreply.github.com>
2021-04-23 18:29:58 -0500

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import JsonData from "./MOCK_DATA.json";
+import styled from 'styled-components';
 import Reactpagine from "react-paginate";
-import Formulario from './components/Formulario';
-import VehicleCard from './components/VehicleCard';
-import Maker_model from './components/Maker_model';
-import CheckBox_MM from './components/CheckBox_MM';
+import Formulario from './components/Formulario.js';
+import VehicleCard from './components/VehicleCard.js';
+import Maker_model from './components/Maker_model.js';
+import CheckBox_MM from './components/CheckBox_MM.js';
 import By_Year from "./components/By_Year";
+import FiltroPrecio from './components/FiltroPrecio.js';
+import Kilometraje from './components/Kilometraje.js';
 import { useState } from 'react';
+
+
+const Styles = styled.div`
+  .App {
+    display: flex;
+    align-items: left;
+  }
+  .wrapper {
+    margin-top: 20vh;
+    width: 35%;
+  }
+`;
+
 
 //App.js
 function App() {
@@ -36,8 +52,23 @@ function App() {
     };
 
   return (
+
     <div className="App">
       {displayUsers}
+      <Styles>
+        <div className="App">
+          <div className="wrapper">
+            <h2>Maximo</h2>
+            <FiltroPrecio color="#0074D9" />
+            <h2>Minimo</h2>
+            <FiltroPrecio color="#3D9970" />
+            <h2>Kilometraje maximo</h2>
+            <Kilometraje color="#0074D9" />
+            <h2>Kilometraje minimo</h2>
+            <Kilometraje color="#3D9970" />
+          </div>
+        </div>
+      </Styles>
       <Reactpagine
         previousLabel = {"Anterior"}
         nextLabel = {"Siguiente"}
