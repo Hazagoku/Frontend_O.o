a8d85574e9d7728a10377692fee0f17b64ed0450
Hazagoku <60244267+Hazagoku@users.noreply.github.com>
2021-05-22 19:42:20 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,9 @@ import React from "react";
 import './App.css';
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Maker_model from "./components/Maker_model";
-import By_Year from "./components/By_Year";
-import By_Price from "./components/By_Price";
-import VehicleCard from "./components/VehicleCard";
-import By_Km from "./components/By_Km";
-import By_Color from "./components/By_Color";
 import Paginacion from "./components/Paginacion";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Formulario from "./components/Formulario";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
 
@@ -21,7 +14,6 @@ function App() {
         <Navbar/>
         <Switch>
           <Route path = "/" exact component={()=><Home/>}/>
-          <Route path = "/VehicleCard" exact component={()=><By_Year/>}/>
           <Route path = "/Formulario" exact component={()=><Formulario/>}/>
           <Route path = "/Paginacion" exact component={()=><Paginacion/>}/>
         </Switch>
